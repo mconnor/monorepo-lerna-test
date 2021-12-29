f9fc0105e462796c4d88c34319fd29417e2b9876
mike connor <mike@rocketnumber9.org>
2021-12-29 10:41:43 -0600

    analyze

--- packages/simple-react-app/src/App.tsx
+++ packages/simple-react-app/src/App.tsx
@@ -1,32 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import logo from './logo.svg'
 import './App.css';
+import {
+    QueryPayload
+} from '@ws/simple-node-server/server'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <button onClick={()=> {
-            fetch("http://localhost:3001", {})
-            .then(res => res.json())
-            .then(data => console.log(data)
-            )
-        }}>get data</button>
-      </header>
-    </div>
-  );
+    const [msg, setMsg] = useState<string>()
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    {msg}
+                </p>
+             
+                <button onClick={() => {
+
+                    fetch("http://localhost:3001/data")
+                        .then(res => res.json())
+                        .then((data: QueryPayload) => setMsg(data.foo))
+                        .catch(err => console.error(err.message)
+                        )
+                }}>Click me - get data</button>
+                <button onClick={() => {
+
+                    setMsg("--------")
+                }}>reset</button>
+            </header>
+        </div>
+    );
 }
-  
+
 export default App;
